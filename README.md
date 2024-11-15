5a706cee6cef92cbb5cc7f8c8bf0037841e1a878
jubaer-emon <jubaer366@gmail.com>
2024-11-15 21:08:17 +0600

    add modular binary exponentiation

--- README.md
+++ README.md
@@ -2,3 +2,4 @@ snippets i use in compettive programming compettitions (codeforces, icpc preli)
 
 # TODO
 - [ ] auto cpp to vscode snippets
+- [ ] graph snippets
---
+++ pwr.cpp
@@ -0,0 +1,11 @@
+ll pwr(ll base, ll power, ll mod=MOD) {
+    base %= mod;
+    ll res = 1;
+    while (power > 0) {
+        if (power & 1)
+            res = res * base % mod;
+        base = base * base % mod;
+        power >>= 1;
+    }
+    return res;
+}
