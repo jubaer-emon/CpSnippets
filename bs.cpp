1ec2c3f5e95b3d2ed77203b41d5a20f1df6ef166
jubaer-emon <jubaer366@gmail.com>
2025-11-12 16:50:08 +0600

    fix: rename function 'f' to 'fn' for consistency

--- bs.cpp
+++ bs.cpp
@@ -3,16 +3,16 @@
 ll binary_search(ll l, ll r) {
     while (r - l > 1) {
         ll m = (l + r) / 2;
-        if (f(m)) {
+        if (fn(m)) {
             r = m; // 0 = f(l) < f(m) = 1
         } else {
             l = m; // 0 = f(m) < f(r) = 1
         }
     }
-    return f(l);                    //return value at first 1
+    return fn(l);                    //return value at first 1
 }
 
-double f() {
+double fn() {
     return ;
 }
 
