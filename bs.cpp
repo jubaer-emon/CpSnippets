7415c99d1849467c27951d2053755de2020259a3
jubaer-emon <jubaer366@gmail.com>
2025-11-12 16:41:38 +0600

    add binary and ternary search implementations

---
+++ bs.cpp
@@ -0,0 +1,18 @@
+
+// f(i) is a boolean function such that f(0) <= ... <= f(n-1)
+ll binary_search(ll l, ll r) {
+    while (r - l > 1) {
+        ll m = (l + r) / 2;
+        if (f(m)) {
+            r = m; // 0 = f(l) < f(m) = 1
+        } else {
+            l = m; // 0 = f(m) < f(r) = 1
+        }
+    }
+    return f(l);                    //return value at first 1
+}
+
+double f() {
+    return ;
+}
+
---
+++ ts.cpp
@@ -0,0 +1,19 @@
+
+double ternary_search(double l, double r) {
+    while (r - l > EPS) {
+        double m1 = l + (r - l) / 3;
+        double m2 = r - (r - l) / 3;
+        double f1 = f(m1);      //evaluates the function at m1
+        double f2 = f(m2);      //evaluates the function at m2
+        if (f1 < f2)
+            l = m1;
+        else
+            r = m2;
+    }
+    return f(l);                    //return the maximum of f(x) in [l, r]
+}
+
+double f() {
+    return ;
+}
+
