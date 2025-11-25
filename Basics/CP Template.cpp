a3fa7174e1dc984b86729fd7ba3ef639378e3003
jubaer-emon <jubaer366@gmail.com>
2025-11-25 17:19:32 +0600

    fix order

--- Basics/CP Template.cpp
+++ Basics/CP Template.cpp
@@ -1,9 +1,9 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-template<typename V, typename T = typename enable_if<!is_same<V, string>::value, typename V::value_type>::type> ostream& operator<<(ostream &os, const V &v) { os << '{'; string sep; for (const T &x : v) os << sep << x, sep = ", "; return os << '}'; }
-template<typename H, typename... T> void dbg_out(H h, T... t) { cerr << ' ' << h; dbg_out(t...); }
 void dbg_out() { cerr << endl; }
+template<typename H, typename... T> void dbg_out(H h, T... t) { cerr << ' ' << h; dbg_out(t...); }
+template<typename V, typename T = typename enable_if<!is_same<V, string>::value, typename V::value_type>::type> ostream& operator<<(ostream &os, const V &v) { os << '{'; string sep; for (const T &x : v) os << sep << x, sep = ", "; return os << '}'; }
 #ifdef LOCAL
 #define dbg(...) cerr << __LINE__ << ":" << #__VA_ARGS__, dbg_out(__VA_ARGS__)
 #else
@@ -17,11 +17,12 @@ void dbg_out() { cerr << endl; }
 using ll = long long;
 using dl = double;
 using vl = vector<ll>;
-using l2 = array<ll,2>;
-using vl2 = vector<l2>;
 using vvl = vector<vl>;
 using vvvl = vector<vvl>;
 template<int SZ> using al = array<ll, SZ>;
+using l2 = al<2>;
+using vl2 = vector<l2>;
+using vvl2 = vector<vl2>;
 
 const int MAXN = 1e5 + 5; // or 1e6 + 5
 const ll MOD = 1e9 + 7;
@@ -34,7 +35,8 @@ int main() {
     cin.tie(0);
     cin.exceptions(cin.failbit);
     
-    int tcs = 1; cin >> tcs;
+    int tcs = 1; 
+    cin >> tcs;
     for (ll tc = 1; tc <= tcs; tc++) {
         dbg(tc);
         
